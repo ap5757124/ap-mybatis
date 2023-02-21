55f96d3a435fc9da56535a4838e0da1c7edd49fc
DESKTOP-POVIV2P\Administrator <1106409687@qq.com>
2023-02-21 23:36:42 +0800

    增加logback日志，动态SQL，缓存

--- src/test/java/mybatis/MyBatisTest.java
+++ src/test/java/mybatis/MyBatisTest.java
@@ -194,6 +194,82 @@ public class MyBatisTest {
             MyBatisUtils.closeSession(session);
         }
     }
+
+    /**
+     * 动态SQL查询
+     * @throws Exception
+     */
+    @Test
+    public void testDynamicSQL() throws Exception {
+        SqlSession session = null;
+        try {
+            session = MyBatisUtils.openSession();
+            Map param = new HashMap();
+            param.put("categoryId", 44);
+            param.put("currentPrice", 500);
+            List<Goods> list = session.selectList("goods.dynamicSQL", param);
+            for (Goods g : list) {
+                System.out.println(g.getTitle() + "——" + g.getCategoryId() + "——" + g.getCurrentPrice());
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(session);
+        }
+    }
+
+    /**
+     * 测试一级缓存 session
+     * @throws Exception
+     */
+    @Test
+    public void testLv1Cache() throws Exception {
+        SqlSession session = null;
+        try {
+            session = MyBatisUtils.openSession();
+            Goods goods = session.selectOne("goods.selectById", 1603);
+            //commit提交时对该namespace缓存强制清空
+            //session.commit();  //如果有commit操作 那么两个对象就不是指向一致了
+            Goods goods1 = session.selectOne("goods.selectById", 1603);
+            //日志只会看到执行一次SQL语句  goods和goods1指向同一个对象 hashCode 一致
+            //session缓存对话没有关闭 第二次会拿第一次的结果
+            System.out.println(goods.hashCode() + ":" + goods1.hashCode());
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(session);
+        }
+    }
+
+    /**
+     * 测试二级缓存
+     * @throws Exception
+     */
+    @Test
+    public void testLv2Cache() throws Exception {
+        SqlSession session = null;
+        try {
+            session = MyBatisUtils.openSession();
+            Goods goods = session.selectOne("goods.selectById", 1603);
+            System.out.println(goods.hashCode());
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(session);
+        }
+        //全局只执行了一次SQL语句 goods
+        // goods - Cache Hit Ratio [goods]: 0.5  日志中表示缓存的利用率
+        try {
+            session = MyBatisUtils.openSession();
+            Goods goods = session.selectOne("goods.selectById", 1603);
+            System.out.println(goods.hashCode());
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(session);
+        }
+    }
+    
 }
 
 
